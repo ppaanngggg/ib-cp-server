0942ca35cc7a627ef2926c9ffc22eb153e2f3c15
ht.pang <hantian.pang@gmail.com>
2023-11-25 22:57:55 +0800

    support auto fill username and password

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -6,28 +6,51 @@ import (
 	"cdr.dev/slog"
 	"github.com/chromedp/chromedp"
 
-	"github.com/ib-gambler/ib-cp-server/pkg"
+	"github.com/ib-gambler/ib-cp-server/pkg/util"
 )
 
 func main() {
 	opts := append(
 		chromedp.DefaultExecAllocatorOptions[:],
 		chromedp.DisableGPU,
-		chromedp.Flag("headless", false),
 		chromedp.IgnoreCertErrors,
 	)
-
 	allocCtx, cancel := chromedp.NewExecAllocator(context.Background(), opts...)
 	defer cancel()
-
 	taskCtx, cancel := chromedp.NewContext(allocCtx)
 	defer cancel()
 
 	err := chromedp.Run(
 		taskCtx,
-		chromedp.Navigate("https://localhost:5000"),
+		chromedp.Tasks{
+			chromedp.Navigate("https://localhost:5000"),
+			// wait for id=xyz-field-username and id=xyz-field-password to appear
+			chromedp.WaitVisible("#xyz-field-username", chromedp.ByID),
+			chromedp.WaitVisible("#xyz-field-password", chromedp.ByID),
+			// wait for button class=btn-primary to appear
+			chromedp.WaitVisible(".btn-primary", chromedp.ByQuery),
+			// enter username and password
+			chromedp.SendKeys(
+				"#xyz-field-username", util.Conf.Account.Username, chromedp.ByID,
+			),
+			chromedp.SendKeys(
+				"#xyz-field-password", util.Conf.Account.Password, chromedp.ByID,
+			),
+			// click the login button
+			chromedp.Click(".btn-primary", chromedp.ByQuery),
+			// wait for class=xyzblock-notification to appear
+			chromedp.WaitVisible(".xyzblock-notification", chromedp.ByQuery),
+			chromedp.ActionFunc(
+				func(ctx context.Context) error {
+					util.Logger.Info(ctx, "Waiting user confirm")
+					return nil
+				},
+			),
+			// wait for class=xyzblock-notification to disappear
+			chromedp.WaitNotPresent(".xyzblock-notification", chromedp.ByQuery),
+		},
 	)
 	if err != nil {
-		pkg.Logger.Fatal(context.Background(), "chromedp.Run", slog.Error(err))
+		util.Logger.Fatal(context.Background(), "chromedp.Run", slog.Error(err))
 	}
 }
--- go.mod
+++ go.mod
@@ -11,7 +11,7 @@ require (
 require (
 	github.com/aymanbagabas/go-osc52/v2 v2.0.1 // indirect
 	github.com/charmbracelet/lipgloss v0.7.1 // indirect
-	github.com/chromedp/cdproto v0.0.0-20231101223124-24f5925b5980 // indirect
+	github.com/chromedp/cdproto v0.0.0-20231114014204-3e458d5176f9 // indirect
 	github.com/chromedp/sysutil v1.0.0 // indirect
 	github.com/gobwas/httphead v0.1.0 // indirect
 	github.com/gobwas/pool v0.2.1 // indirect
